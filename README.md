cee35ab49528c482be7a3c39d1a5c242fd23f2b9
Ayman Nageeb <aimangta123121@gmail.com>
2022-05-25 13:20:38 +0200

    Feature: set up cd pipline

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 ## Wayn Location Rental services
 
 this the code of the backend written in PHP Laravel framework
-
-test
---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+
+vendor/bin/phpunit
+
+ 
+
+(git push) || true
+
+ 
+
+git checkout main
+
+git merge develop
+
+ 
+
+git push origin main
+
+ 
+
+git checkout develop
